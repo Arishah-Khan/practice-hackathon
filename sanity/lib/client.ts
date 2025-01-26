229b3d1ff397b7fa0d11fada3cb1c156b0d28951
Arishah Khan <arishahsanaullah78@gmail.com>
2025-01-26 08:44:18 +0500

    Addtion

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false,
-  // token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
